9a530caa4ca9fdc9916e702b4359890b9a98f366
Nithilan-su57 <nithilan1207@gmail.com>
2026-04-20 22:55:27 +0530

    ran into errors and solved it.

--- backend/src/main/java/com/gym/gym_managment/model/Member.java
+++ backend/src/main/java/com/gym/gym_managment/model/Member.java
@@ -10,19 +10,6 @@ import java.time.temporal.ChronoUnit;
 // class; it's a database table."
 public class Member extends Person{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    //@Id and @GeneratedValue: This creates an
-    // ID column that automatically increases (1, 2, 3...) so you don't have to assign IDs manually.
-    private long id;
-
-    @Column(nullable = false)
-    //tells the database generation to create not null values , if it does it will throw SQL exception.
-    private String name;
-    @Column(unique = true, nullable = false)
-    //ensures no two rows in the database table can have the same values ,if it does it will throw an exception.
-    private String email;
-    private String phoneNumber;
     private String membershipPlan;
     private LocalDate joinDate;
     private LocalDate expiryDate;
@@ -77,37 +64,6 @@ public class Member extends Person{
         if (this.expiryDate == null){ return false;}
         return !this.expiryDate.isBefore(LocalDate.now());
     }
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 
     public String getMembershipPlan() {
         return membershipPlan;
@@ -125,8 +81,11 @@ public class Member extends Person{
         this.joinDate = joinDate;
     }
 
-    public LocalDate getExpiryDate() {return expiryDate;}
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
 
-    public void setExpiryDate(LocalDate expiryDate) {this.expiryDate = expiryDate;}
-
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
 }
--- backend/src/main/java/com/gym/gym_managment/model/Person.java
+++ backend/src/main/java/com/gym/gym_managment/model/Person.java
@@ -5,9 +5,19 @@ import jakarta.persistence.*;
 @MappedSuperclass // This tells JPA to include these fields in the child tables
 public abstract class Person {
 
-    private String name;
-    private String email;
-    private String phoneNumber;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Id and @GeneratedValue: This creates an
+    // ID column that automatically increases (1, 2, 3...) so you don't have to assign IDs manually.
+    protected long id;
+
+    @Column(nullable = false)
+    //tells the database generation to create not null values , if it does it will throw SQL exception.
+    protected String name;
+    @Column(unique = true, nullable = false)
+    //ensures no two rows in the database table can have the same values ,if it does it will throw an exception.
+    protected String email;
+    protected String phoneNumber;
 
     public Person() {}
 
@@ -25,7 +35,6 @@ public abstract class Person {
         System.out.println("Phone Number : " + this.phoneNumber);
     }
 
-    // Getters and Setters (standard boilerplate)
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
     public String getEmail() { return email; }
